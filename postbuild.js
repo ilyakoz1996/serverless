397b82471c59e5b7e848787fd238f83dd860b0b2
ilyakoz1996 <ilyakoz1996@gmail.com>
2024-03-12 18:14:27 +0800

    api

--- postbuild.js
+++ postbuild.js
@@ -3,7 +3,7 @@ const updateLinks = async () => {
   const SERVER_URL = `https://${process.env.NEXT_PUBLIC_VERCEL_URL}/api`;
   const CLIENT_URL = `https://${process.env.NEXT_PUBLIC_VERCEL_URL}`;
   const CLIENT_ID = process.env.NEXT_PUBLIC_CLIENT_ID;
-  const AUTH_SERVER_URL = "https://s-auth.org";
+  const AUTH_SERVER_URL = "https://api.s-auth.org";
 
   const url = `${AUTH_SERVER_URL}/updateApp?clientId=${CLIENT_ID}&websiteUrl=${CLIENT_URL}&callbackUrl=${SERVER_URL}/api/auth?callback=true`;
   console.log("url: ", url);
