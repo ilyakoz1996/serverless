288e90d327f0da086f70b0bbd65189ed453c1926
ilyakoz1996 <ilyakoz1996@gmail.com>
2024-03-12 18:05:03 +0800

    fetch update

--- postbuild.js
+++ postbuild.js
@@ -1,12 +1,19 @@
-const SERVER_URL = `https://${process.env.NEXT_PUBLIC_VERCEL_URL}/api`
-const CLIENT_URL = `https://${process.env.NEXT_PUBLIC_VERCEL_URL}`
-const CLIENT_ID = process.env.NEXT_PUBLIC_CLIENT_ID
-const AUTH_SERVER_URL = "https://s-auth.org"
-
 const updateLinks = async () => {
-    const response = await fetch(`${AUTH_SERVER_URL}/updateApp?clientId=${CLIENT_ID}&websiteUrl=${CLIENT_URL}&callbackUrl=${SERVER_URL}/api/auth?callback=true`);
-    const updateUrl = await response.json();
-    return updateUrl
-}
 
-updateLinks()
+  const SERVER_URL = `https://${process.env.NEXT_PUBLIC_VERCEL_URL}/api`;
+  const CLIENT_URL = `https://${process.env.NEXT_PUBLIC_VERCEL_URL}`;
+  const CLIENT_ID = process.env.NEXT_PUBLIC_CLIENT_ID;
+  const AUTH_SERVER_URL = "https://s-auth.org";
+
+  const url = `${AUTH_SERVER_URL}/updateApp?clientId=${CLIENT_ID}&websiteUrl=${CLIENT_URL}&callbackUrl=${SERVER_URL}/api/auth?callback=true`;
+  console.log("url: ", url);
+  try {
+      const response = fetch(url);
+      return response;
+  } catch (err) {
+    console.log(err)
+    return 
+  }
+};
+
+updateLinks();
